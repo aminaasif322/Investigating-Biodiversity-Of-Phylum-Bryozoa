201c406a390699a087677c3b36d52b49e522168c
Brandon Spoja <73257088+bspoja@users.noreply.github.com>
2020-11-10 21:41:45 -0500

    Repleaced the lines of code that calculated the total number of species with the created function UniqueTotal. This block of code was the same code that was used previously, so using the created function reduced repetition.

--- improved version of assignment1.R
+++ improved version of assignment1.R
@@ -105,12 +105,10 @@ class(dfbryozoa.sp.rich)
 dfbryozoa.f.aus <- filter(dfbryozoa.sub, country %in% c('Australia'))
 
 #Now I will create an object 'bryozoa.aus.u.s' and will use unique function which will separate out unique species only.
-bryozoa.aus.u.s <- unique(dfbryozoa.f.aus$species_name)
-length(bryozoa.aus.u.s)
-bryozoa.aus.na <- unique(na.omit(bryozoa.aus.u.s))
-bryozoa.aus.na
-length(bryozoa.aus.na)
-class(bryozoa.aus.na)
+#Use created function UniqueTotal to find the total unique species names in the data set dfbryozoa.f.aus and assign it to a new variable called bryozoa.aus.u.s. This function reduced 5 lines of code to 2 lines of code.
+bryozoa.aus.u.s <- UniqueTotal(dfbryozoa.f.aus$species_name)
+bryozoa.aus.u.s
+
 #Above analysis shows that Initially there were 15 unique no of species but after removing NA it becomes 14.
 
 #Now we will do further analysis. I have created an object 'bryozoa.div.aus' which will contain only species analysed from Australia So, I have created an object 'bryozoa.div.aus1' in which NA's are removed and then diversity is calculated.
